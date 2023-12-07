e7c08fe17bf26209f4b30101257bd4a117b7d5c1
ananta khodake <ananta.khodake02@gmail.com>
2023-12-07 12:49:00 +0530

    Genaraltab ui is compleate in account setting

--- src/component/dashboard/settings/Settingtabs.js
+++ src/component/dashboard/settings/Settingtabs.js
@@ -1,21 +1,27 @@
-import React from 'react'
-import "./Settingtabs.css"
-import Genaralsettingtab from './Genaralsettingtab'
+import React, { useState } from "react";
+import "./Settingtabs.css";
+import Genaralsettingtab from "./Genaralsettingtab";
 
 function Settingtabs() {
+  const [tab, setTab] = useState("Genaral");
   return (
-    <div className='settingtabs_container'>
-       <div className="s_card1">
-        <h4>Genaral</h4>
-        <p>Security</p>
-        <p>Default Privacy</p>
-        <p>Notifications</p>
-        <p>Convert Account</p>
-        <p>Deactivate Account</p>
+    <div className="settingtabs_container">
+      <div className="s_card1">
+        <button
+          onClick={() => setTab("Genaral")}
+          className={` ${tab == "Genaral" ? "active" : ""}`}
+        >
+          Genaral
+        </button>
+        <button>Security</button>
+        <button>Default Privacy</button>
+        <button>Convert Account</button>
+        <button>Convert Account</button>
+        <button>Deactivate Acc</button>
       </div>
       <Genaralsettingtab />
     </div>
-  )
+  );
 }
 
-export default Settingtabs
+export default Settingtabs;
